b5ba0e986752f8a6ffde0b524ecbed547e06fee1
Mike Dilley <mike.dilley@gmail.com>
2018-12-18 22:25:36 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181218221914) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "passwords"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
