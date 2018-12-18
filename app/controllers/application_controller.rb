c8f5840b86a0f157b0a96a842599f30330959851
Mike Dilley <mike.dilley@gmail.com>
2018-12-18 22:43:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,6 +40,11 @@ class ApplicationController < Sinatra::Base
     ##your code here
     user = User.find_by(:username => params[:username], :password => params[:password])
     binding.pry
+    if !!user
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
   end
 
   get "/failure" do
